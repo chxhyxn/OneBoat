ba0daaf8378993b6b7a35f0d06721c01e3f81ee6
chxhyxn <chxhyxn@gmail.com>
2025-03-17 19:02:17 +0900

    Initial Commit

---
+++ OneBoat/OneBoatApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneBoatApp.swift
+//  OneBoat
+//
+//  Created by SeanCho on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct OneBoatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OneBoatTests/OneBoatTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneBoatTests.swift
+//  OneBoatTests
+//
+//  Created by SeanCho on 3/17/25.
+//
+
+import Testing
+@testable import OneBoat
+
+struct OneBoatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
